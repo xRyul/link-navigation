999b74ced4c6ca29b47019daa5ad00a5be304bb7
xRyul <daniel.alesko@gmail.com>
2024-09-07 10:24:33 +0100

    Debug Mobile 1

--- src/main.ts
+++ src/main.ts
@@ -47,6 +47,7 @@ export default class LinkNavigationPlugin extends Plugin {
     private readonly CLEANUP_NOTICE_COOLDOWN = 5000; // 5 seconds cooldown
 
     async onload() {
+        new Notice ("LinkNavigationPlugin: Starting to load");
         await this.loadSettings();
         this.addSettingTab(new LinkNavigationSettingTab(this.app, this));
 
